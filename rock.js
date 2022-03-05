a9edb7aeffcc07c2138bb310cd023b6752bb5cb9
Codingluxo <luis.garcia@ug.uchile.cl>
2022-03-05 06:54:52 -0300

    comments added

--- rock.js
+++ rock.js
@@ -1,22 +1,33 @@
-let juegoComputadora = ["rock","paper","scissors"];
+let computerArray = ["rock","paper","scissors"];
 let scorePlayer = 0;
 let scoreComputer = 0;
 
+/* 
+
+I searched in Stackoverflow ways to get a random value from an array, I found this link, first I used Math.random function
+(this return a number between 0 and 1), then I multiply this value with the lenght of my array (in this case, 3) and finally
+I run this result in the function Math.floor, which round downwards the final number, so the possible values are 0, 1 and 2 
+https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array
+
+*/
 
 function computerPlay(){
 
-const random = juegoComputadora[Math.floor(Math.random() * juegoComputadora.length)];
+const random = computerArray[Math.floor(Math.random() * computerArray.length)];
 return random;
 
 }
 
 function singleRound(playerSelection,computerSelection){
 
+    // I need to ensure that the input value is correct
+    if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors"){
 
-   
+        console.log ("wrong choice");
+    }
 
-
-    if(playerSelection == computerSelection) {
+    // keep record of the current score.
+    else if(playerSelection == computerSelection) {
 
         console.log("It's a tie, keep playing",scorePlayer,scoreComputer);
        
@@ -65,9 +76,7 @@ function singleRound(playerSelection,computerSelection){
 
         scorePlayer += 1; 
         console.log("Well done, scissors beats paper",scorePlayer,scoreComputer);
-        
-
-        
+                
     }
 
 }
@@ -77,12 +86,12 @@ function game () {
 
     for(let i = 0; i < 5; i++){
 
-        playerSelection = prompt("Choose Rock,Paper or Scissors").toLowerCase();
+        playerSelection = prompt("Choose Rock,Paper or Scissors").toLowerCase(); // case-insensitive
         computerSelection = computerPlay();
-        singleRound(playerSelection,computerSelection) 
+        singleRound(playerSelection,computerSelection); 
         if( scoreComputer == 3 || scorePlayer == 3){ 
             
-            // Best of five, so the one who gets to three wins the game
+            // Best of five, so the first one who gets to three wins the game
             break;
         }
 
@@ -90,9 +99,11 @@ function game () {
      
     }
 
+    //display final result
+
     if(scorePlayer > scoreComputer){
         
-        //display final result
+        
 
         console.log ("Congratulations, you just win Rock, paper and scissors");
     }
@@ -107,3 +118,5 @@ function game () {
         console.log("It's a tie, thank you for playing.");
     }
 }
+
+game ();
